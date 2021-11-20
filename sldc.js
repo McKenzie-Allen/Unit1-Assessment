4e3ee99f3e0c2f9ec90ec0bc316380e62bc4d569
McKenzie Allen <mckenzie.allen144@gmail.com>
2021-11-20 14:48:22 -0700

    defined the steps as const

--- sldc.js
+++ sldc.js
@@ -21,3 +21,6 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = "This is where you take what was planned in the Planning phase and find the best way to implement the plan. We break it down into tasks and features to achieve what the end goal is. What is the MPV, what is the minimum basic function of the product?"
+const design = "This is where you decide what the product going to look like, feel like and how is this going to be used. What is going to draw users to the site and what is going to keep them there? is the UI going to be intuitive, what is the flow of the website?"
+const Implementation = ""
